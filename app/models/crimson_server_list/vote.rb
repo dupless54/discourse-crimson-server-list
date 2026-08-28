960378baa32be7eb3a9f363de2d061250f327ad9
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 21:18:42 +0300

    FIX: add generated schema annotation for votes

--- app/models/crimson_server_list/vote.rb
+++ app/models/crimson_server_list/vote.rb
@@ -14,3 +14,21 @@ module ::CrimsonServerList
     validates :server_id, uniqueness: { scope: %i[user_id voted_on] }
   end
 end
+
+# == Schema Information
+#
+# Table name: crimson_server_votes
+#
+#  id         :bigint           not null, primary key
+#  voted_on   :date             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  server_id  :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  idx_crimson_server_daily_vote            (server_id,user_id,voted_on) UNIQUE
+#  index_crimson_server_votes_on_server_id  (server_id)
+#  index_crimson_server_votes_on_user_id    (user_id)
+#
