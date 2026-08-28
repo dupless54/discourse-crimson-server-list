15c22f78bfa66b9a01fce1a8868dec97161e7f55
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 22:12:45 +0300

    FIX: guard bigint foreign key rollback

--- db/migrate/20260828000001_widen_crimson_server_foreign_keys.rb
+++ db/migrate/20260828000001_widen_crimson_server_foreign_keys.rb
@@ -6,12 +6,20 @@ class WidenCrimsonServerForeignKeys < ActiveRecord::Migration[7.0]
     crimson_server_reviews
     crimson_server_claim_requests
   ].freeze
+  INTEGER_MAX = 2_147_483_647
 
   def up
     TABLES.each { |table| change_column table, :server_id, :bigint, null: false }
   end
 
   def down
-    TABLES.each { |table| change_column table, :server_id, :integer, null: false }
+    TABLES.each do |table|
+      if select_value("SELECT 1 FROM #{quote_table_name(table)} WHERE server_id > #{INTEGER_MAX} LIMIT 1")
+        raise ActiveRecord::IrreversibleMigration,
+              "#{table}.server_id contains values that cannot fit in a 32-bit integer"
+      end
+
+      change_column table, :server_id, :integer, null: false
+    end
   end
 end
